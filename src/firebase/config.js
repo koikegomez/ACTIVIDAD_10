aec91bd2487229fc5028f8da72aab66c533b45ae
Jorge Gallegos <a1121250017@utch.edu.mx>
2022-03-30 17:23:36 -0700

    Primera subida

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD4KaJ-MArSxzaByKuzyaM7QU1rBTRwt78',
-  authDomain: 'actividad-10-utch.firebaseapp.com',
-  databaseURL: 'https://actividad-10-utch-default-rtdb.firebaseio.com',
-  projectId: 'actividad-10-utch',
-  storageBucket: 'actividad-10-utch.appspot.com',
-  messagingSenderId: '912457574451',
-  appId: '1:912457574451:web:27ad1971316a81d0595b46',
+  apiKey: "AIzaSyCF1D06Tq6wjg2sQjHaGF8BcNU83f-F8g4",
+  authDomain: "actividad-10-utch-1f81c.firebaseapp.com",
+  databaseURL: "https://actividad-10-utch-1f81c-default-rtdb.firebaseio.com",
+  projectId: "actividad-10-utch-1f81c",
+  storageBucket: "actividad-10-utch-1f81c.appspot.com",
+  messagingSenderId: "662187196512",
+  appId: "1:662187196512:web:adab6991633e1b15504305"
 };
 
 if (!firebase.apps.length) {
